a74e3f948a2474cc203baa5ec84eafed7050d8a8
m-aciek <maciej.olko@gmail.com>
2016-06-14 17:04:36 +0200

    Added 'success' admin notice handling

--- templates/list-table-view.php
+++ templates/list-table-view.php
@@ -3,6 +3,9 @@
     <?php if (array_key_exists('error', $_GET)): ?>
         <div class="notice notice-error"><p><?= $_GET['error']; ?></p></div>
     <?php endif; ?>
+    <?php if (array_key_exists('success', $_GET)): ?>
+        <div class="notice notice-success"><p><?= $_GET['success']; ?></p></div>
+    <?php endif; ?>
 
     <form method="post">
         <input type="hidden" name="page" value="ttest_list_table">
