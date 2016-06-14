93e6e37927896ae1383acf39b6c09a4a9d2855b4
m-aciek <maciej.olko@gmail.com>
2016-06-14 15:48:55 +0200

    Update list-table-view.php
    Added admin notice in case of error.

--- templates/list-table-view.php
+++ templates/list-table-view.php
@@ -1,6 +1,8 @@
 <div class="wrap">
     <h2><?php _e( '%heading%', '%textdomain%' ); ?> <a href="<?php echo admin_url( 'admin.php?page=%PAGENAME%&action=new' ); ?>" class="add-new-h2"><?php _e( 'Add New', '%textdomain%' ); ?></a></h2>
-
+    <?php if (array_key_exists('error', $_GET)): ?>
+        <div class="notice notice-error"><p><?= $_GET['error']; ?></p></div>
+    <?php endif; ?>
     <form method="post">
         <input type="hidden" name="page" value="ttest_list_table">
 
