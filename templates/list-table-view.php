f95780b17e0fa2f05cff444baf79e8d5efdff7b9
m-aciek <maciej.olko@gmail.com>
2016-06-29 01:08:32 +0200

    Added success/error message on item creation
    * Update list-table-view.php
    
    Added admin notice in case of error.
    
    * Minor correction
    
    Preserved line break.
    
    * Improving message handling
    
    Changed query args from 'message' to 'error' and 'success'.
    
    * Added 'success' admin notice handling
    
    * Shorthands replaced to `echo`

--- templates/list-table-view.php
+++ templates/list-table-view.php
@@ -1,5 +1,11 @@
 <div class="wrap">
     <h2><?php _e( '%heading%', '%textdomain%' ); ?> <a href="<?php echo admin_url( 'admin.php?page=%PAGENAME%&action=new' ); ?>" class="add-new-h2"><?php _e( 'Add New', '%textdomain%' ); ?></a></h2>
+    <?php if (array_key_exists('error', $_GET)): ?>
+        <div class="notice notice-error"><p><?php echo $_GET['error']; ?></p></div>
+    <?php endif; ?>
+    <?php if (array_key_exists('success', $_GET)): ?>
+        <div class="notice notice-success"><p><?php echo $_GET['success']; ?></p></div>
+    <?php endif; ?>
 
     <form method="post">
         <input type="hidden" name="page" value="ttest_list_table">
